9b36b1fc94a3a6b051ad94b89d42eb28e98f8e52
Yegyu Han <yegyuhan@gmail.com>
2025-05-21 21:32:58 +0900

    update license tools

--- tools/main_custom_upright.py
+++ tools/main_custom_upright.py
@@ -1,3 +1,21 @@
+# This file is part of a project that includes code adapted from BlenderProc (GPL-3.0).
+#
+# Copyright (C) 2021 German Aerospace Center (DLR)
+# Modified by  Sojeong Kim, 2025
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import blenderproc as bproc
 import argparse
 import os
